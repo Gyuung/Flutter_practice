526234036390dae825b3e6281cb62e055ed3cbb5
Gyuung <zaqrhkd2@naver.com>
2022-04-13 14:39:27 +0900

    캐릭터위젯

---
+++ character_widget/android/app/src/main/kotlin/com/example/character_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.character_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ character_widget/lib/main.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+// import 'package:tflite/tflite.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UNG',
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('SableAntelope'),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'NAME',
+                style: const TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'WINWIN',
+                style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+// String res = await Tflite.loadModel(
+//   model: "assets/converted_model.tflite",
+//   labels: "assets/labels.txt",
+//   numThreads: 1, // defaults to 1
+//   isAsset: true, // defaults to true, set to false to load resources outside assets
+//   useGpuDelegate: false // defaults to false, set to true to use GPU delegate
+// );
